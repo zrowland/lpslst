1ad1c0417ca6ba0108bbd9b8538fa03098d17b02
Zack Rowland <zrowland@gmail.com>
2018-05-11 12:52:03 -0600

    Initial commit

---
+++ lpslst.ps1
@@ -0,0 +1,341 @@
+﻿# Script for identifying log sources that are potentially being collected under the incorrect log source type
+# Zack Rowland, LogRhythm Strategic Integrations, 2018
+# Version 2.0, 5/11/18
+
+param(
+    [Parameter()][string]$FilePath,
+    [Parameter()][string]$FolderPath,
+    [Parameter()][string]$OutputFilePath,
+    [Parameter()][string]$CatchAllPercent
+)
+
+# ┌────────────────────────────────────────────────────────────┐
+# │    Primary Variables/Regex Objects                         │
+# └────────────────────────────────────────────────────────────┘
+
+# Default threshold for finding misconfigured log source types is 50% hit rate on the LST's "Catch All : Level 1" rule. This can be overridden with the "-CatchAllPercent" parameter
+
+[decimal]$ca_pct = 50.0
+if ($CatchAllPercent -ne "")
+{
+    $ca_pct = [System.Convert]::ToDecimal($CatchAllPercent)
+}
+
+$rgxstr_lst = 'Log\sSource\sType:\s([^\r\n]+)'
+$rgxstr_block = 'Base-rule[^\r\n]+[\r\n]+[^\r\n]+[\r\n]+Log\sSource\sType:\s([^\r\n]+)[\r\n]+(.*?)(?=(?:Base-rule|$))'
+$rgxstr_catchall = 'Catch All : Level 1\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)'
+$rgx_lst = New-Object System.Text.RegularExpressions.Regex -ArgumentList $($rgxstr_lst, [System.Text.RegularExpressions.RegexOptions]::None)
+$rgx_block = New-Object System.Text.RegularExpressions.Regex -ArgumentList ($rgxstr_block, [System.Text.RegularExpressions.RegexOptions]::Singleline)
+$rgx_catchall = New-Object System.Text.RegularExpressions.Regex -ArgumentList ($rgxstr_catchall, [System.Text.RegularExpressions.RegexOptions]::None)
+$global:ofile_path = ""
+[System.IO.FileStream]$global:ofile = $null
+[System.IO.StreamWriter]$global:sw = $null
+[System.Boolean]$flags_ofile = $false
+
+# ┌────────────────────────────────────────────────────────────┐
+# │ Main                                                       │
+# └────────────────────────────────────────────────────────────┘
+
+Write-Host "Starting..."
+
+if ($FilePath -ne "" -and $FolderPath -ne "")
+{
+    Write-Error "Cannot specify -FilePath and -FolderPath at same time, pick one or the other."
+}
+else
+{
+    if ($OutputFilePath -ne "")
+    {
+        if ([System.IO.Path]::IsPathRooted($OutputFilePath) -eq $false)
+        {
+            $ofile_path = [System.IO.Path]::Combine($PSScriptRoot,$OutputFilePath)
+        }
+        else
+        {
+            $ofile_path = $OutputFilePath
+        }
+        try
+        {
+            $ofile = New-Object System.IO.FileStream -ArgumentList $($ofile_path,[System.IO.FileMode]::CreateNew,[System.IO.FileAccess]::ReadWrite)
+            $sw = New-Object System.IO.StreamWriter -ArgumentList $ofile
+            $flags_ofile = $true
+        }
+        catch
+        {
+            Write-Error $Error[0]
+            exit
+        }
+    }
+
+    if($FilePath -ne "")
+    {
+        if ([System.IO.Path]::IsPathRooted($FilePath) -eq $false)
+        {
+            $file_path = [System.IO.Path]::Combine($PSScriptRoot, $FilePath)
+        }
+        else
+        {
+            $file_path = $FilePath
+        }
+        
+        $hit_list = New-Object System.Collections.Generic.List[string]
+		$log_text = [System.IO.File]::ReadAllText([System.IO.Path]::GetFullPath($file_path))
+		
+		$match_lst = $rgx_block.Matches($log_text)
+		
+		foreach ($m in $match_lst)
+		{
+		    $tLogSourceType = $m.Groups[1].Value
+		    $tLstBlock = $m.Groups[0].Value
+		
+		    if ($rgx_catchall.IsMatch($tLstBlock) -eq $true)
+		    {
+		        $match_catchall = $rgx_catchall.Match($tLstBlock)
+		        $tHitRateStr = $match_catchall.Groups[1].Value
+		        $dHitRate = [System.Convert]::ToDecimal($tHitRateStr)
+		
+		        if ($dHitRate -gt $ca_pct)
+		        {
+                    if ($hit_list.Contains($tLogSourceType) -eq $false)
+                    {
+                        $hit_list.Add($tLogSourceType)
+
+                        if ($flags_ofile -eq $true)
+                        {
+                            $sw.WriteLine($tLogSourceType)
+                        }
+                        else
+                        {
+                            echo $tLogSourceType
+                        }
+                    }
+		        }
+		    }
+		}
+
+        Write-Host "Done!"
+
+        if ($flags_ofile -eq $true)
+        {
+            $sw.Close()
+            $ofile.Close()
+        }
+
+        exit
+    }
+    elseif($FolderPath -ne "")
+    {
+        $tp = [System.IO.Path]::Combine($PSScriptRoot, $FolderPath)
+        $log_list = [System.IO.Directory]::EnumerateFiles($tp)
+        $log_queue = New-Object System.Collections.Generic.Queue[string] -ArgumentList ([System.Collections.IEnumerable]$log_list)
+        $hit_list = New-Object System.Collections.Generic.List[string]
+
+        while ($log_queue.Count -gt 0)
+        {
+            $log_path = $log_queue.Dequeue()
+            $log_text = [System.IO.File]::ReadAllText([System.IO.Path]::GetFullPath($log_path))
+			
+			$match_lst = $rgx_block.Matches($log_text)
+			
+			foreach ($m in $match_lst)
+			{
+			    $tLogSourceType = $m.Groups[1].Value
+			    $tLstBlock = $m.Groups[0].Value
+			
+			    if ($rgx_catchall.IsMatch($tLstBlock) -eq $true)
+			    {
+			        $match_catchall = $rgx_catchall.Match($tLstBlock)
+			        $tHitRateStr = $match_catchall.Groups[1].Value
+			        $dHitRate = [System.Convert]::ToDecimal($tHitRateStr)
+			
+			        if ($dHitRate -gt $ca_pct)
+			        {
+                        if ($hit_list.Contains($tLogSourceType) -eq $false)
+                        {
+                            $hit_list.Add($tLogSourceType)
+
+                            if ($flags_ofile -eq $true)
+                            {
+                                $sw.WriteLine($tLogSourceType)
+                            }
+                            else
+                            {
+                                echo $tLogSourceType
+                            }
+                        }
+			        }
+			    }
+			}
+        }
+
+        Write-Host "Done!"
+
+        if ($flags_ofile -eq $true)
+        {
+            $sw.Close()
+            $ofile.Close()
+        }
+
+        exit
+    }
+    else
+    {
+        Write-Error "Hmm this error should never occur, please check your parameters and try again..."
+
+        exit
+    }
+}
+# SIG # Begin signature block
+# MIIcdQYJKoZIhvcNAQcCoIIcZjCCHGICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUwBzd2N+rnHBxvhF8wlJiFExp
+# zySgghebMIID7jCCA1egAwIBAgIQfpPr+3zGTlnqS5p31Ab8OzANBgkqhkiG9w0B
+# AQUFADCBizELMAkGA1UEBhMCWkExFTATBgNVBAgTDFdlc3Rlcm4gQ2FwZTEUMBIG
+# A1UEBxMLRHVyYmFudmlsbGUxDzANBgNVBAoTBlRoYXd0ZTEdMBsGA1UECxMUVGhh
+# d3RlIENlcnRpZmljYXRpb24xHzAdBgNVBAMTFlRoYXd0ZSBUaW1lc3RhbXBpbmcg
+# Q0EwHhcNMTIxMjIxMDAwMDAwWhcNMjAxMjMwMjM1OTU5WjBeMQswCQYDVQQGEwJV
+# UzEdMBsGA1UEChMUU3ltYW50ZWMgQ29ycG9yYXRpb24xMDAuBgNVBAMTJ1N5bWFu
+# dGVjIFRpbWUgU3RhbXBpbmcgU2VydmljZXMgQ0EgLSBHMjCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBALGss0lUS5ccEgrYJXmRIlcqb9y4JsRDc2vCvy5Q
+# WvsUwnaOQwElQ7Sh4kX06Ld7w3TMIte0lAAC903tv7S3RCRrzV9FO9FEzkMScxeC
+# i2m0K8uZHqxyGyZNcR+xMd37UWECU6aq9UksBXhFpS+JzueZ5/6M4lc/PcaS3Er4
+# ezPkeQr78HWIQZz/xQNRmarXbJ+TaYdlKYOFwmAUxMjJOxTawIHwHw103pIiq8r3
+# +3R8J+b3Sht/p8OeLa6K6qbmqicWfWH3mHERvOJQoUvlXfrlDqcsn6plINPYlujI
+# fKVOSET/GeJEB5IL12iEgF1qeGRFzWBGflTBE3zFefHJwXECAwEAAaOB+jCB9zAd
+# BgNVHQ4EFgQUX5r1blzMzHSa1N197z/b7EyALt0wMgYIKwYBBQUHAQEEJjAkMCIG
+# CCsGAQUFBzABhhZodHRwOi8vb2NzcC50aGF3dGUuY29tMBIGA1UdEwEB/wQIMAYB
+# Af8CAQAwPwYDVR0fBDgwNjA0oDKgMIYuaHR0cDovL2NybC50aGF3dGUuY29tL1Ro
+# YXd0ZVRpbWVzdGFtcGluZ0NBLmNybDATBgNVHSUEDDAKBggrBgEFBQcDCDAOBgNV
+# HQ8BAf8EBAMCAQYwKAYDVR0RBCEwH6QdMBsxGTAXBgNVBAMTEFRpbWVTdGFtcC0y
+# MDQ4LTEwDQYJKoZIhvcNAQEFBQADgYEAAwmbj3nvf1kwqu9otfrjCR27T4IGXTdf
+# plKfFo3qHJIJRG71betYfDDo+WmNI3MLEm9Hqa45EfgqsZuwGsOO61mWAK3ODE2y
+# 0DGmCFwqevzieh1XTKhlGOl5QGIllm7HxzdqgyEIjkHq3dlXPx13SYcqFgZepjhq
+# IhKjURmDfrYwggSjMIIDi6ADAgECAhAOz/Q4yP6/NW4E2GqYGxpQMA0GCSqGSIb3
+# DQEBBQUAMF4xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jwb3Jh
+# dGlvbjEwMC4GA1UEAxMnU3ltYW50ZWMgVGltZSBTdGFtcGluZyBTZXJ2aWNlcyBD
+# QSAtIEcyMB4XDTEyMTAxODAwMDAwMFoXDTIwMTIyOTIzNTk1OVowYjELMAkGA1UE
+# BhMCVVMxHTAbBgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTQwMgYDVQQDEytT
+# eW1hbnRlYyBUaW1lIFN0YW1waW5nIFNlcnZpY2VzIFNpZ25lciAtIEc0MIIBIjAN
+# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAomMLOUS4uyOnREm7Dv+h8GEKU5Ow
+# mNutLA9KxW7/hjxTVQ8VzgQ/K/2plpbZvmF5C1vJTIZ25eBDSyKV7sIrQ8Gf2Gi0
+# jkBP7oU4uRHFI/JkWPAVMm9OV6GuiKQC1yoezUvh3WPVF4kyW7BemVqonShQDhfu
+# ltthO0VRHc8SVguSR/yrrvZmPUescHLnkudfzRC5xINklBm9JYDh6NIipdC6Anqh
+# d5NbZcPuF3S8QYYq3AhMjJKMkS2ed0QfaNaodHfbDlsyi1aLM73ZY8hJnTrFxeoz
+# C9Lxoxv0i77Zs1eLO94Ep3oisiSuLsdwxb5OgyYI+wu9qU+ZCOEQKHKqzQIDAQAB
+# o4IBVzCCAVMwDAYDVR0TAQH/BAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDAO
+# BgNVHQ8BAf8EBAMCB4AwcwYIKwYBBQUHAQEEZzBlMCoGCCsGAQUFBzABhh5odHRw
+# Oi8vdHMtb2NzcC53cy5zeW1hbnRlYy5jb20wNwYIKwYBBQUHMAKGK2h0dHA6Ly90
+# cy1haWEud3Muc3ltYW50ZWMuY29tL3Rzcy1jYS1nMi5jZXIwPAYDVR0fBDUwMzAx
+# oC+gLYYraHR0cDovL3RzLWNybC53cy5zeW1hbnRlYy5jb20vdHNzLWNhLWcyLmNy
+# bDAoBgNVHREEITAfpB0wGzEZMBcGA1UEAxMQVGltZVN0YW1wLTIwNDgtMjAdBgNV
+# HQ4EFgQURsZpow5KFB7VTNpSYxc/Xja8DeYwHwYDVR0jBBgwFoAUX5r1blzMzHSa
+# 1N197z/b7EyALt0wDQYJKoZIhvcNAQEFBQADggEBAHg7tJEqAEzwj2IwN3ijhCcH
+# bxiy3iXcoNSUA6qGTiWfmkADHN3O43nLIWgG2rYytG2/9CwmYzPkSWRtDebDZw73
+# BaQ1bHyJFsbpst+y6d0gxnEPzZV03LZc3r03H0N45ni1zSgEIKOq8UvEiCmRDoDR
+# EfzdXHZuT14ORUZBbg2w6jiasTraCXEQ/Bx5tIB7rGn0/Zy2DBYr8X9bCT2bW+IW
+# yhOBbQAuOA2oKY8s4bL0WqkBrxWcLC9JG9siu8P+eJRRw4axgohd8D20UaF5Mysu
+# e7ncIAkTcetqGVvP6KUwVyyJST+5z3/Jvz4iaGNTmr1pdKzFHTx/kuDDvBzYBHUw
+# ggTKMIIDsqADAgECAhA7fcSpOOvoChwkFo65IyOmMA0GCSqGSIb3DQEBCwUAMH8x
+# CzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jwb3JhdGlvbjEfMB0G
+# A1UECxMWU3ltYW50ZWMgVHJ1c3QgTmV0d29yazEwMC4GA1UEAxMnU3ltYW50ZWMg
+# Q2xhc3MgMyBTSEEyNTYgQ29kZSBTaWduaW5nIENBMB4XDTE3MDQwNDAwMDAwMFoX
+# DTIwMDQwNDIzNTk1OVowYjELMAkGA1UEBhMCVVMxETAPBgNVBAgMCENvbG9yYWRv
+# MRAwDgYDVQQHDAdCb3VsZGVyMRYwFAYDVQQKDA1Mb2dSaHl0aG0gSW5jMRYwFAYD
+# VQQDDA1Mb2dSaHl0aG0gSW5jMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
+# AQEArr9SaqNn81S+mF151igpNeqvzWs40uPSf5tXu9iQUqXCWx25pECOcNk7W/Z5
+# O9dXiQmdIvIFF5FqCkP6rzYtKx3OH9xIzoSlOKTxRWj3wo+R1vxwT9ThOvYiz/5T
+# G5TJZ1n4ILFTd5JexoS9YTA7tt+2gbDtjKLBorYUCvXv5m6PREHpZ0uHXGCDWrJp
+# zhiYQdtyAfxGQ6J9SOekYu3AiK9Wf3nbuoxLDoeEQ4boFW3iQgYJv1rRFA1k4AsT
+# nsxDmEhd9enLZEQd/ikkYrIwkPVN9rPH6B+uRsBxIWIy1PXHwyaCTO0HdizjQlhS
+# RaV/EzzbyTMPyWNluUjLWe0C4wIDAQABo4IBXTCCAVkwCQYDVR0TBAIwADAOBgNV
+# HQ8BAf8EBAMCB4AwKwYDVR0fBCQwIjAgoB6gHIYaaHR0cDovL3N2LnN5bWNiLmNv
+# bS9zdi5jcmwwYQYDVR0gBFowWDBWBgZngQwBBAEwTDAjBggrBgEFBQcCARYXaHR0
+# cHM6Ly9kLnN5bWNiLmNvbS9jcHMwJQYIKwYBBQUHAgIwGQwXaHR0cHM6Ly9kLnN5
+# bWNiLmNvbS9ycGEwEwYDVR0lBAwwCgYIKwYBBQUHAwMwVwYIKwYBBQUHAQEESzBJ
+# MB8GCCsGAQUFBzABhhNodHRwOi8vc3Yuc3ltY2QuY29tMCYGCCsGAQUFBzAChhpo
+# dHRwOi8vc3Yuc3ltY2IuY29tL3N2LmNydDAfBgNVHSMEGDAWgBSWO1PweTOXr32D
+# 7y4rzMq3hh5yZjAdBgNVHQ4EFgQUf2bE5CWM4/1XmNZgr/W9NahQJkcwDQYJKoZI
+# hvcNAQELBQADggEBAHfeSWKiWK1eI+cD/1z/coADJfCnPynzk+eY/MVh0jOGM2dJ
+# eu8MBcweZdvjv4KYN/22Zv0FgDbwytBFgGxBM6pSRU3wFJN9XroLJCLAKCmyPN7H
+# IIaGp5RqkeL4jgKpB5R6NqSb3ES9e2obzpOEvq49nPCSCzdtv+oANVYj7cIxwBon
+# VvIqOZFxM9Bj6tiMDwdvtm0y47LQXM3+gWUHNf5P7M8hAPw+O2t93hPmd2xA3+U7
+# FqUAkhww4IhdIfaJoxNPDjQ4dU+dbYL9BaDfasYQovY25hSe66a9S9blz9Ew2uNR
+# iGEvYMyxaDElEXfyDSTnmR5448q1jxFpY5giBY0wggTTMIIDu6ADAgECAhAY2tGe
+# Jn3ou0ohWM3MaztKMA0GCSqGSIb3DQEBBQUAMIHKMQswCQYDVQQGEwJVUzEXMBUG
+# A1UEChMOVmVyaVNpZ24sIEluYy4xHzAdBgNVBAsTFlZlcmlTaWduIFRydXN0IE5l
+# dHdvcmsxOjA4BgNVBAsTMShjKSAyMDA2IFZlcmlTaWduLCBJbmMuIC0gRm9yIGF1
+# dGhvcml6ZWQgdXNlIG9ubHkxRTBDBgNVBAMTPFZlcmlTaWduIENsYXNzIDMgUHVi
+# bGljIFByaW1hcnkgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkgLSBHNTAeFw0wNjEx
+# MDgwMDAwMDBaFw0zNjA3MTYyMzU5NTlaMIHKMQswCQYDVQQGEwJVUzEXMBUGA1UE
+# ChMOVmVyaVNpZ24sIEluYy4xHzAdBgNVBAsTFlZlcmlTaWduIFRydXN0IE5ldHdv
+# cmsxOjA4BgNVBAsTMShjKSAyMDA2IFZlcmlTaWduLCBJbmMuIC0gRm9yIGF1dGhv
+# cml6ZWQgdXNlIG9ubHkxRTBDBgNVBAMTPFZlcmlTaWduIENsYXNzIDMgUHVibGlj
+# IFByaW1hcnkgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkgLSBHNTCCASIwDQYJKoZI
+# hvcNAQEBBQADggEPADCCAQoCggEBAK8kCAgpejWeYAyq50s7Ttx8vDxFHLsr4P4p
+# AvlXCKNkhRUn9fGtyDGJXSLoKqqmQrOP+LlVt7G3S7P+j34HV+zvQ9tmYhVhz2AN
+# pNje+ODDYgg9VBPrScpZVIUm5SuPG5/r9aGRwjNJ2ENjalJL0o/ocFFN0Ylpe8dw
+# 9rPcEnTbe11LVtOWvxV3obD0oiXyrxySZxjl9AYE75C55ADk3Tq1Gf8CuvQ87uCL
+# 6zeL7PTXrPL28D2v3XWRMxkdHEDLdCQZIZPZFP6sKlLHj9UESeSNY0eIPGmDy/5H
+# vSt+T8WVrg6d1NFDwGdz4xQIfuU/n3O4MwrPXT80h5aK7lPoJRUCAwEAAaOBsjCB
+# rzAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjBtBggrBgEFBQcBDARh
+# MF+hXaBbMFkwVzBVFglpbWFnZS9naWYwITAfMAcGBSsOAwIaBBSP5dMahqyNjmvD
+# z4Bq1EgYLHsZLjAlFiNodHRwOi8vbG9nby52ZXJpc2lnbi5jb20vdnNsb2dvLmdp
+# ZjAdBgNVHQ4EFgQUf9Nlp8Ld7LvwMAnzQzn6Aq8zMTMwDQYJKoZIhvcNAQEFBQAD
+# ggEBAJMkSjBfYs/YGpgvPercmS29d/aleSI47MSnoHgSrWIORXBkxeeXZi2YCX5f
+# r9bMKGXyAaoIGkfe+fl8kloIaSAN2T5tbjwNbtjmBpFAGLn4we3f20Gq4JYgyc1k
+# FTiByZTuooQpCxNvjtsM3SUC26SLGUTSQXoFaUpYT2DKfoJqCwKqJRc5tdt/54Rl
+# KpWKvYbeXoEWgy0QzN79qIIqbSgfDQvE5ecaJhnh9BFvELWV/OdCBTLbzp1RXii2
+# noXTW++lfUVAco63DmsOBvszNUhxuJ0ni8RlXw2GdpxEevaVXPZdMggzpFS2GD9o
+# XPJCSoU4VINf0egs8qwR1qjtY2owggVZMIIEQaADAgECAhA9eNf5dklgsmF99PAe
+# yoYqMA0GCSqGSIb3DQEBCwUAMIHKMQswCQYDVQQGEwJVUzEXMBUGA1UEChMOVmVy
+# aVNpZ24sIEluYy4xHzAdBgNVBAsTFlZlcmlTaWduIFRydXN0IE5ldHdvcmsxOjA4
+# BgNVBAsTMShjKSAyMDA2IFZlcmlTaWduLCBJbmMuIC0gRm9yIGF1dGhvcml6ZWQg
+# dXNlIG9ubHkxRTBDBgNVBAMTPFZlcmlTaWduIENsYXNzIDMgUHVibGljIFByaW1h
+# cnkgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkgLSBHNTAeFw0xMzEyMTAwMDAwMDBa
+# Fw0yMzEyMDkyMzU5NTlaMH8xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRl
+# YyBDb3Jwb3JhdGlvbjEfMB0GA1UECxMWU3ltYW50ZWMgVHJ1c3QgTmV0d29yazEw
+# MC4GA1UEAxMnU3ltYW50ZWMgQ2xhc3MgMyBTSEEyNTYgQ29kZSBTaWduaW5nIENB
+# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl4MeABavLLHSCMTXaJNR
+# YB5x9uJHtNtYTSNiarS/WhtR96MNGHdou9g2qy8hUNqe8+dfJ04LwpfICXCTqdpc
+# DU6kDZGgtOwUzpFyVC7Oo9tE6VIbP0E8ykrkqsDoOatTzCHQzM9/m+bCzFhqghXu
+# PTbPHMWXBySO8Xu+MS09bty1mUKfS2GVXxxw7hd924vlYYl4x2gbrxF4GpiuxFVH
+# U9mzMtahDkZAxZeSitFTp5lbhTVX0+qTYmEgCscwdyQRTWKDtrp7aIIx7mXK3/nV
+# jbI13Iwrb2pyXGCEnPIMlF7AVlIASMzT+KV93i/XE+Q4qITVRrgThsIbnepaON2b
+# 2wIDAQABo4IBgzCCAX8wLwYIKwYBBQUHAQEEIzAhMB8GCCsGAQUFBzABhhNodHRw
+# Oi8vczIuc3ltY2IuY29tMBIGA1UdEwEB/wQIMAYBAf8CAQAwbAYDVR0gBGUwYzBh
+# BgtghkgBhvhFAQcXAzBSMCYGCCsGAQUFBwIBFhpodHRwOi8vd3d3LnN5bWF1dGgu
+# Y29tL2NwczAoBggrBgEFBQcCAjAcGhpodHRwOi8vd3d3LnN5bWF1dGguY29tL3Jw
+# YTAwBgNVHR8EKTAnMCWgI6Ahhh9odHRwOi8vczEuc3ltY2IuY29tL3BjYTMtZzUu
+# Y3JsMB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEFBQcDAzAOBgNVHQ8BAf8EBAMC
+# AQYwKQYDVR0RBCIwIKQeMBwxGjAYBgNVBAMTEVN5bWFudGVjUEtJLTEtNTY3MB0G
+# A1UdDgQWBBSWO1PweTOXr32D7y4rzMq3hh5yZjAfBgNVHSMEGDAWgBR/02Wnwt3s
+# u/AwCfNDOfoCrzMxMzANBgkqhkiG9w0BAQsFAAOCAQEAE4UaHmmpN/egvaSvfh1h
+# U/6djF4MpnUeeBcj3f3sGgNVOftxlcdlWqeOMNJEWmHbcG/aIQXCLnO6SfHRk/5d
+# yc1eA+CJnj90Htf3OIup1s+7NS8zWKiSVtHITTuC5nmEFvwosLFH8x2iPu6H2aZ/
+# pFalP62ELinefLyoqqM9BAHqupOiDlAiKRdMh+Q6EV/WpCWJmwVrL7TJAUwnewus
+# GQUioGAVP9rJ+01Mj/tyZ3f9J5THujUOiEn+jf0or0oSvQ2zlwXeRAwV+jYrA9zB
+# UAHxoRFdFOXivSdLVL4rhF4PpsN0BQrvl8OJIrEfd/O9zUPU8UypP7WLhK9k8tAU
+# ITGCBEQwggRAAgEBMIGTMH8xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRl
+# YyBDb3Jwb3JhdGlvbjEfMB0GA1UECxMWU3ltYW50ZWMgVHJ1c3QgTmV0d29yazEw
+# MC4GA1UEAxMnU3ltYW50ZWMgQ2xhc3MgMyBTSEEyNTYgQ29kZSBTaWduaW5nIENB
+# AhA7fcSpOOvoChwkFo65IyOmMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQow
+# CKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcC
+# AQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQ3ZpIyjSc9J64Bb6It
+# MnrpdF2lLTANBgkqhkiG9w0BAQEFAASCAQBF0EwCbZ5ddEgs1g4T0NazGY1aS85L
+# TZPJX4z8UirWVQ9cWLx+VR9e4Vs+yTDPVcggiqn1emM5Q/zvp1TYKaOpqInERFnl
+# E0BkRDtjCd+eUMpsyjHSCB0UvENXdLgjnc6dgGyvd8aIM08M+hmwy4IHvsZ6odo6
+# Bf6Z+bTfbCxe3EYzxZnu7vbfIB1NfvoJn/uQAEcOI6tZQ/sA+gMEcKWSOIkuCkSQ
+# OVGMm4jGqe13trHGzH5xF6v+D3EaYUl3IeEW3aG6voBqE3BU3fItVsfhcdfu7dB1
+# HxrrOAbm8MrLPuazvcglMlYzAIfGTl/+nimWMgFjDP45xVXFWo6/Cki2oYICCzCC
+# AgcGCSqGSIb3DQEJBjGCAfgwggH0AgEBMHIwXjELMAkGA1UEBhMCVVMxHTAbBgNV
+# BAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTAwLgYDVQQDEydTeW1hbnRlYyBUaW1l
+# IFN0YW1waW5nIFNlcnZpY2VzIENBIC0gRzICEA7P9DjI/r81bgTYapgbGlAwCQYF
+# Kw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkF
+# MQ8XDTE4MDUxMTE4NDg0OVowIwYJKoZIhvcNAQkEMRYEFNuPn7dVeHJ7/QieD4jR
+# /eKaN2iAMA0GCSqGSIb3DQEBAQUABIIBADtzfVkrtx/0GMXtXstDJcWpbCWlzLEt
+# 1t8vjcENHndIBH/lK1dfqvrk0AlUJPBq8inmaU4EJE4I6woP9L7rlwjE1Qmxn1Br
+# aq67PtaB9/lExoLWlyxqj7feNJn6UEQ1axKzIkLIA9EX93p+Y9PnTDws2NZZGLxa
+# D2DgRI0fZXK7RDWpeLyREHuHDsghfVz4nBSSvCj9vCdTuSTGjNOzPKji3zOsz8e4
+# zIHYJ+bTckLYW0WVNGDHr0Q3xIvJ3ZFJKOfKP62fR9IVese/exr/8S7YvtbxWsdr
+# iBXxg1D0ty/uKggIz/X7vfW3gM2zEZTXfpzBuoSFRov+FGbAkDDWoYc=
+# SIG # End signature block
